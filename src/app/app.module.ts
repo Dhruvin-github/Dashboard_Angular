4d4a7c89de1bd31a6ec6bb608f0b38cdb6b4e89b
Hiren Makwana <hirenmakwana4403@gmail.com>
2021-12-26 13:53:10 +0530

    commit form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProcuctListComponent } from './Dashboard/Product/procuct-list/procuct-list.component';
 import { ProductDetailComponent } from './Dashboard/Product/product-detail/product-detail.component';
-import { ProductMediaComponent } from './Dashboard/product/product-media/product-media.component';
+import { ProductMediaComponent } from './Dashboard/Product/product-media/product-media.component';
 
 @NgModule({
   declarations: [
